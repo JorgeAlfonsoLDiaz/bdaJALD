b4359417990d9417ccb57123ba4782fd0e01a52f
JorgeAlfonsoLDiaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-05-12 14:08:22 +0000

    Se creó README.md
    Se creó un nuevo archivo con el nombre de README.md

--- main.sql
+++ main.sql
@@ -1,8 +1,12 @@
-CREATE TABLE PERSONAS(
-id_persona integer PRIMARY KEY AUTOINCREMENT,
-nombre varchar(25) NOT NULL,
-primer_apellido varchar(25),
-segundo_apellido varchar(25),
-email varchar(25));
+CREATE TABLE personas(
+  id_persona integer PRIMARY KEY AUTOINCREMENT,
+  nombre varchar(25) NOT NULL,
+  primer_apellido varchar(25),
+  segundo_apellido varchar(25),
+  email varchar(25));
 
+INSERT INTO personas (nombre, primer_apellido, segundo_apellido, email) VALUES 
+  ('Nombre1', 'PrimerAp1', 'SegundoAp1', 'email1@mail.com'),
+  ('Nombre2', 'PrimerAp2', 'SegundoAp2', 'email2@mail.com'),
+  ('Nombre3', 'PrimerAp3', 'SegundoAp3', 'email3@mail.com');
 .show
