9c03c4abfc16d2a78e2827075b6be19f2c97cd84
JorgeAlfonsoLDi <19818068-JorgeAlfonsoLDi@users.noreply.replit.com>
2023-05-12 13:47:28 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PERSONAS(
+id_persona integer PRIMARY KEY AUTOINCREMENT,
+nombre varchar(25) NOT NULL,
+primer_apellido varchar(25),
+segundo_apellido varchar(25),
+email varchar(25));
+
+.show
