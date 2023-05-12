3dd45ebffb95c567d4cb93b593c6643f4285b014
JorgeAlfonsoLDiaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-05-12 14:09:47 +0000

    MODIFY main.sql

--- main.sql
+++ main.sql
@@ -9,4 +9,6 @@ INSERT INTO personas (nombre, primer_apellido, segundo_apellido, email) VALUES
   ('Nombre1', 'PrimerAp1', 'SegundoAp1', 'email1@mail.com'),
   ('Nombre2', 'PrimerAp2', 'SegundoAp2', 'email2@mail.com'),
   ('Nombre3', 'PrimerAp3', 'SegundoAp3', 'email3@mail.com');
+
+SELECT * FROM personas;
 .show
